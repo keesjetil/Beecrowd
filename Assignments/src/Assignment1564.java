2cf6419b78564d8335d499364098a5228927a5c7
Kees Til <kees.til@hotmail.com>
2024-03-04 22:30:23 +0100

    added 1564 and 1589

---
+++ Assignments/src/Assignment1564.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Assignment1564 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        while(scn.hasNextLine()) {
+            var N = Integer.parseInt(scn.nextLine());
+            if(N == 0){
+                System.out.println("vai ter copa!");
+            }else{
+                System.out.println("vai ter duas!");
+
+            }
+        }
+
+    }
+}
---
+++ Assignments/src/Assignment1589.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Assignment1589 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        var N = scn.nextInt();
+        for(int i =0; i<N; i++){
+            var A = scn.nextInt();
+            var B = scn.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
