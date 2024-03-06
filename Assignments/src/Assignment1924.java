2b2cb89854ac8da577dcaa05b884fce2a3ff1cdf
Kees Til <kees.til@hotmail.com>
2024-03-06 21:46:25 +0100

    added stuff

---
+++ Assignments/src/Assignment1924.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Assignment1924 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Ciencia da Computacao");
+
+    }
+}
