7d8860743b86eace606563c5d68b8e00db036b46
Kees Til <kees.til@hotmail.com>
2024-03-25 11:59:05 +0100

    Added assignments

---
+++ Assignments/src/Assignment3091.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Assignment3091 {
+    public static void main(String[] args) {
+        var scn = new Scanner(System.in);
+        var A = scn.nextInt();
+        var B = scn.nextInt();
+        System.out.println(A % B);
+    }
+}
