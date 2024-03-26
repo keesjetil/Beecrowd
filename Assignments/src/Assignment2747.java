2c99af1291fd6a54a008f80babbb9c60ffce532b
Kees Til <kees.til@hotmail.com>
2024-03-26 16:15:51 +0100

    Added assignments

---
+++ Assignments/src/Assignment2747.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Assignment2747 {
+    public static void main(String[] args) {
+        System.out.println("-".repeat(39));
+        System.out.printf("|%s|%n", " ".repeat(37));
+        System.out.printf("|%s|%n", " ".repeat(37));
+        System.out.printf("|%s|%n", " ".repeat(37));
+        System.out.printf("|%s|%n", " ".repeat(37));
+        System.out.printf("|%s|%n", " ".repeat(37));
+        System.out.println("-".repeat(39));
+    }
+}
---
+++ Assignments/src/Assignment2748.java
@@ -0,0 +1,13 @@
+public class Assignment2748 {
+    public static void main(String[] args) {
+        System.out.println("-".repeat(39));
+        System.out.printf("|%s%s|%n", "x = 35"," ".repeat(31));
+        System.out.printf("|%s|%n", " ".repeat(37));
+        System.out.printf("|%s%s%s|%n", " ".repeat(15), "x = 35", " ".repeat(16));
+        System.out.printf("|%s|%n", " ".repeat(37));
+        System.out.printf("|%s%s|%n", " ".repeat(31),"x = 35");
+        System.out.println("-".repeat(39));
+        System.out.println("···············".length());
+        System.out.println("················".length());
+    }
+}
